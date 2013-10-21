a58e06d2c59037355219bb1e0b616d142981b9a2
moralesalberto <alberto@moralitos.com>
2013-10-21 16:24:45 -0400

    starting again with haml

---
+++ README.md
@@ -0,0 +1,4 @@
+Web
+===
+
+A website
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Web::Application.config.secret_key_base = 'f5f2a5f3d2c7b25c1b9c1d77bc4a25643beeb74805360262e68eebc07c128fe162929362ac1b557e8f450e25a18bfb7f8319d85a3aee9860fd4d775284c077fa'
